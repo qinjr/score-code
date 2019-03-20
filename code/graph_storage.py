87547c38b0fb202f715648873a33cd48b055eb99
qinjr <qinjr@icloud.com>
2019-03-20 09:57:16 +0800

    cal stat

--- code/graph_storage.py
+++ code/graph_storage.py
@@ -2,6 +2,10 @@ import pymongo
 import pickle as pkl
 import datetime
 import time
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 
 SECONDS_PER_DAY = 24 * 3600
 DATA_DIR = '../../score-data/CCMR/feateng/'
@@ -74,11 +78,31 @@ class CCMRGraphStore(GraphStore):
         self.item_coll.insert_many(item_doc_list)
         print('item collection completed')
 
+    def cal_stat(self, user_hist_file, item_hist_file):
+        # calculate user doc
+        hist_len_user = []
+        for i in range(1, self.user_num + 1):
+            start_t = time.time()
+            user_doc = self.user_coll.find_one({'uid': i})
+            for t in range(self.time_idx_num):
+                hist_len_user.append(len(user_doc['hist_%d'%(t)]))
+            print('one user time: {}'.format(time.time() - start_t))
+        plt.hist(hist_len_user, bins=range(max(hist_len_user)+1))
+        plt.savefig(user_hist_file)
 
+        # calculate item doc
+        hist_len_item = []
+        for i in range(self.user_num + 1, self.user_num + self.item_num + 1):
+            item_doc = self.item_coll.find_one({'iid': i})
+            for t in range(self.time_idx_num):
+                hist_len_item.append(len(item_doc['hist_%d'%(t)]))
+        plt.hist(hist_len_item, bins=range(max(hist_len_item)+1))
+        plt.savefig(item_hist_file)
 
 
 if __name__ == "__main__":
     # For CCMR
     gs = CCMRGraphStore(DATA_DIR + 'remap_rating_pos.csv', DATA_DIR + 'remap_movie_info_dict.pkl')
-    gs.construct_coll()
-    # gs.write2db()
+    # gs.construct_coll()
+    gs.cal_stat(DATA_DIR + 'user_hist_len_distri.png', DATA_DIR + 'item_hist_len_distri.png')
+    
