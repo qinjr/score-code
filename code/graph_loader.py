d3213699f5f8f382ce4d4e9079327dad0ec08dea
qinjr <qinjr@icloud.com>
2019-03-25 11:37:07 +0800

    minor

--- code/graph_loader.py
+++ code/graph_loader.py
@@ -217,8 +217,7 @@ class GraphLoader(object):
                     label_batch.append(1)
                 else:
                     label_batch.append(0)
-        return user_1hop_batch, user_2hop_batch, item_1hop_batch, item_2hop_batch, 
-                target_user_batch, target_item_batch, label_batch
+        return user_1hop_batch, user_2hop_batch, item_1hop_batch, item_2hop_batch, target_user_batch, target_item_batch, label_batch
 
 
 
