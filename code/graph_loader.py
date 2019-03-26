9d6823a05aa493ed152e4427936bcd9c8b0a3922
qinjr <qinjr@icloud.com>
2019-03-26 23:59:04 +0800

    minor

--- code/graph_loader.py
+++ code/graph_loader.py
@@ -229,21 +229,21 @@ class GraphLoader(object):
     def gen_user_history(self, start_uid):
         user_1hop, user_2hop = [], []
         for i in range(START_TIME, self.pred_time):
-            # t = time.time()
+            t = time.time()
             user_1hop_t, user_2hop_t = self.gen_node_neighbor(start_uid, 'user', i)
             user_1hop.append(user_1hop_t)
             user_2hop.append(user_2hop_t)
-            # print('user time per slice: {}'.format(time.time() - t))
+            print('user time per slice: {}'.format(time.time() - t))
         return user_1hop, user_2hop
 
     def gen_item_history(self, start_iid):
         item_1hop, item_2hop = [], []
         for i in range(START_TIME, self.pred_time):
-            # t = time.time()
+            t = time.time()
             item_1hop_t, item_2hop_t = self.gen_node_neighbor(start_iid, 'item', i)
             item_1hop.append(item_1hop_t)
             item_2hop.append(item_2hop_t)
-            # print('item time per slice: {}'.format(time.time() - t))
+            print('item time per slice: {}'.format(time.time() - t))
         return item_1hop, item_2hop
         
     def __next__(self):
@@ -318,12 +318,12 @@ if __name__ == "__main__":
     # graph_loader.gen_target_file(TIME_SLICE_NUM_CCMR - 2, NEG_SAMPLE_NUM, DATA_DIR_CCMR + 'target_train.txt')
     # graph_loader.gen_target_file(TIME_SLICE_NUM_CCMR - 1, NEG_SAMPLE_NUM, DATA_DIR_CCMR + 'target_test.txt')
     t = time.time()
-    st = time.time()
-    i = 0
+    # st = time.time()
+    # i = 0
     for batch_data in graph_loader:
         # print(batch_data[-3:])
         print('batch time: {}'.format(time.time() - t))
         t = time.time()
-        i += 1
-        if i == 100:
-            print('average time:{}'.format((time.time() - st)/100))
+        # i += 1
+        # if i == 100:
+        #     print('average time:{}'.format((time.time() - st)/100))
