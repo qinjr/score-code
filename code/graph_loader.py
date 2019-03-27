80bc2b5059ba8e2b34ec803631d43f04e8d2cecf
qinjr <qinjr@icloud.com>
2019-03-27 11:45:29 +0800

    minor

--- code/graph_loader.py
+++ code/graph_loader.py
@@ -228,22 +228,23 @@ class GraphLoader(object):
             
     def gen_user_history(self, start_uid):
         user_1hop, user_2hop = [], []
+        t = time.time()
         for i in range(START_TIME, self.pred_time):
-            t = time.time()
             user_1hop_t, user_2hop_t = self.gen_node_neighbor(start_uid, 'user', i)
             user_1hop.append(user_1hop_t)
             user_2hop.append(user_2hop_t)
-            print('user time per slice: {}'.format(time.time() - t))
+        print('gen_user_history time: {}'.format(time.time() - t))
         return user_1hop, user_2hop
 
     def gen_item_history(self, start_iid):
         item_1hop, item_2hop = [], []
+        t = time.time()
         for i in range(START_TIME, self.pred_time):
-            t = time.time()
+            
             item_1hop_t, item_2hop_t = self.gen_node_neighbor(start_iid, 'item', i)
             item_1hop.append(item_1hop_t)
             item_2hop.append(item_2hop_t)
-            print('item time per slice: {}'.format(time.time() - t))
+        print('gen_item_history time: {}'.format(time.time() - t))
         return item_1hop, item_2hop
         
     def __next__(self):
