9230267fd79ef4f9d2d6b86211667fcf4e8034fd
qinjr <qinjr@icloud.com>
2019-03-28 22:23:15 +0800

    minor

--- code/graph_loader.py
+++ code/graph_loader.py
@@ -114,6 +114,7 @@ class GraphLoader(object):
                 return
             if self.work_q.empty() == False and self.worker_begin.value == 1:
                 start_node_id, node_type, time_slice = self.work_q.get()
+                t=time.time()
                 # print(start_node_id, node_type, time_slice, name)
                 if node_type == 'user':
                     start_node_doc = user_coll.find({'uid': start_node_id})[0]
@@ -143,6 +144,7 @@ class GraphLoader(object):
                     self.queue_2hop.put((node_2hop_dummy, time_slice))
                     with self.work_cnt.get_lock():
                         self.work_cnt.value += 1
+                    print('cal time: {}'.format(time.time-t))
                     # return node_1hop_dummy, node_2hop_dummy
                 else:
                     # deal with 1hop
@@ -190,19 +192,21 @@ class GraphLoader(object):
                         self.queue_2hop.put((node_2hop_t, time_slice))
                         with self.work_cnt.get_lock():
                             self.work_cnt.value += 1
+                        print('cal time: {}'.format(time.time-t))
                     else:
                         self.queue_1hop.put((node_1hop_t, time_slice))
                         self.queue_2hop.put((node_2hop_dummy, time_slice))
                         with self.work_cnt.get_lock():
                             self.work_cnt.value += 1
+                        print('cal time: {}'.format(time.time-t))
                         # return node_1hop_t, node_2hop_dummy
             else:
-                time.sleep(0.1)
+                time.sleep(0.01)
     
     def gen_user_history(self, start_uid):
         while True:
             if not self.work_q.empty():
-                time.sleep(0.1)
+                time.sleep(0.01)
             if self.work_q.empty() and self.worker_begin.value == 0:
                 for i in range(self.pred_time):
                     self.work_q.put((start_uid, 'user', i))
@@ -233,12 +237,14 @@ class GraphLoader(object):
     def gen_item_history(self, start_iid):
         while True:
             if not self.work_q.empty():
-                time.sleep(0.1)
+                time.sleep(0.01)
             if self.work_q.empty() and self.worker_begin.value == 0:
+                t = time.time()
                 for i in range(self.pred_time):
                     self.work_q.put((start_iid, 'item', i))
                 with self.worker_begin.get_lock():
                     self.worker_begin.value = 1
+                print('produce time: {}'.format(time.time()-t))
             if self.work_q.empty() and self.worker_begin.value == 1 and self.work_cnt.value == self.pred_time:
                 print('begin summary')
                 t = time.time()
@@ -260,7 +266,7 @@ class GraphLoader(object):
                     self.worker_begin.value = 0
                 with self.work_cnt.get_lock():
                     self.work_cnt.value = 0
-                print('time: {}'.format(time.time()-t))
+                print('result time: {}'.format(time.time()-t))
                 return item_1hop, item_2hop
     
     def __iter__(self):
