48a882128a7a8c44afd98c6bb2da025641e00d37
qinjr <qinjr@icloud.com>
2019-03-29 23:02:19 +0800

    split database to chunks

--- code/graph_storage.py
+++ code/graph_storage.py
@@ -9,6 +9,8 @@ import numpy as np
 
 SECONDS_PER_DAY = 24 * 3600
 DATA_DIR_CCMR = '../../score-data/CCMR/feateng/'
+USER_PER_COLLECTION = 100000
+ITEM_PER_COLLECTION = 100000
 
 class GraphStore(object):
     def __init__(self):
@@ -44,39 +46,78 @@ class CCMRGraphStore(GraphStore):
     def gen_item_doc(self, iid):
         item_doc = {}
         item_doc['iid'] = iid
-        item_doc['did'], item_doc['aid'], item_doc['gid'], item_doc['nid'] = self.movie_info_dict[str(iid)]
+        # item_doc['did'], item_doc['aid'], item_doc['gid'], item_doc['nid'] = self.movie_info_dict[str(iid)]
         for t in range(self.time_slice_num):
             item_doc['hist_%d'%t] = []
         return item_doc
 
     def construct_coll(self):
-        collist = self.db.list_collection_names()
-        if 'user' not in collist:
-            print('begin initializing user collection')
-            # construct user collection
-            user_doc_list = []
-            for i in range(1, self.user_num + 1):
-                user_doc_list.append(self.gen_user_doc(i))
-            print('initialize user doc list completed')
+        list_of_user_doc_list = []
+        list_of_item_doc_list = []
 
-        if 'item' not in collist:
-            print('begin initializing item collection')
-            # construct item collection
+        user_coll_num = self.user_num // USER_PER_COLLECTION
+        if self.user_num % USER_PER_COLLECTION != 0:
+            user_coll_num += 1
+        item_coll_num = self.item_num // ITEM_PER_COLLECTION
+        if self.item_num % ITEM_PER_COLLECTION != 0:
+            item_coll_num += 1
+
+        for i in range(user_coll_num):
+            user_doc_list = []
+            for uid in range(i * USER_PER_COLLECTION + 1, (i + 1) * USER_PER_COLLECTION + 1):
+                user_doc_list.append(self.gen_user_doc(uid))
+            list_of_user_doc_list.append(user_doc_list)
+
+        for i in range(item_coll_num):
             item_doc_list = []
-            for i in range(self.user_num + 1, self.user_num + self.item_num + 1):
-                item_doc_list.append(self.gen_item_doc(i))
-            print('initialize item doc list completed')
-        
+            for iid in range(i * ITEM_PER_COLLECTION + 1 + self.user_num, (i + 1) * ITEM_PER_COLLECTION + 1 + self.user_num):
+                item_doc_list.append(self.gen_item_doc(iid))
+            list_of_item_doc_list.append(item_doc_list)
+
         for line in self.rating_file:
             uid, iid, _, t_idx = line[:-1].split(',')
-            user_doc_list[int(uid) - 1]['hist_%s'%(t_idx)].append(int(iid))
-            item_doc_list[int(iid) - self.user_num - 1]['hist_%s'%(t_idx)].append(int(uid))
+            list_of_user_doc_list[(int(uid) - 1) // USER_PER_COLLECTION][(int(uid) - 1) % USER_PER_COLLECTION]['hist_%s'%(t_idx)].append(int(iid))
+            list_of_item_doc_list[(int(iid) - self.user_num - 1) // ITEM_PER_COLLECTION][(int(iid) - self.user_num - 1) % ITEM_PER_COLLECTION]['hist_%s'%(t_idx)].append(int(uid))
+            # user_doc_list[int(uid) - 1]['hist_%s'%(t_idx)].append(int(iid))
+            # item_doc_list[int(iid) - self.user_num - 1]['hist_%s'%(t_idx)].append(int(uid))
         print('user and item doc list completed')
 
-        self.user_coll.insert_many(user_doc_list)
+        for i in range(len(list_of_user_doc_list)):
+            db['user_%d'%(i)].insert_many(list_of_user_doc_list[i])
         print('user collection completed')
-        self.item_coll.insert_many(item_doc_list)
+        for i in range(len(list_of_item_doc_list)):
+            db['item_%d'%(i)].insert_many(list_of_item_doc_list[i])
         print('item collection completed')
+
+        # insert user collections
+        
+        # collist = self.db.list_collection_names()
+        # if 'user' not in collist:
+        #     print('begin initializing user collection')
+        #     # construct user collection
+        #     user_doc_list = []
+        #     for i in range(1, self.user_num + 1):
+        #         user_doc_list.append(self.gen_user_doc(i))
+        #     print('initialize user doc list completed')
+
+        # if 'item' not in collist:
+        #     print('begin initializing item collection')
+        #     # construct item collection
+        #     item_doc_list = []
+        #     for i in range(self.user_num + 1, self.user_num + self.item_num + 1):
+        #         item_doc_list.append(self.gen_item_doc(i))
+        #     print('initialize item doc list completed')
+        
+        # for line in self.rating_file:
+        #     uid, iid, _, t_idx = line[:-1].split(',')
+        #     user_doc_list[int(uid) - 1]['hist_%s'%(t_idx)].append(int(iid))
+        #     item_doc_list[int(iid) - self.user_num - 1]['hist_%s'%(t_idx)].append(int(uid))
+        # print('user and item doc list completed')
+
+        # self.user_coll.insert_many(user_doc_list)
+        # print('user collection completed')
+        # self.item_coll.insert_many(item_doc_list)
+        # print('item collection completed')
 
     def cal_stat(self):
         # calculate user doc
@@ -121,4 +162,4 @@ if __name__ == "__main__":
     # For CCMR
     gs = CCMRGraphStore(DATA_DIR_CCMR + 'remap_rating_pos_idx.csv', DATA_DIR_CCMR + 'remap_movie_info_dict.pkl')
     gs.construct_coll()
-    gs.cal_stat()
+    # gs.cal_stat()
