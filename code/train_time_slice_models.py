1e31419744e0970eda9c71fd17da55562b741667
qinjr <qinjr@icloud.com>
2019-04-02 19:17:27 +0800

    new train/test splitting

--- code/train_time_slice_models.py
+++ code/train_time_slice_models.py
@@ -191,8 +191,8 @@ if __name__ == '__main__':
         graph_handler_params = [TIME_SLICE_NUM_CCMR, 'ccmr_2hop', OBJ_PER_TIME_SLICE_CCMR, \
                                 USER_NUM_CCMR, ITEM_NUM_CCMR, 1, 5, START_TIME_CCMR, None, \
                                 DATA_DIR_CCMR + 'remap_movie_info_dict.pkl']
-        target_file_train = DATA_DIR_CCMR + 'target_train.txt'
-        target_file_test = DATA_DIR_CCMR + 'target_test_sample.txt'
+        target_file_train = DATA_DIR_CCMR + 'target_train_splitbyuser_41.txt'#'target_train.txt'
+        target_file_test = DATA_DIR_CCMR + 'target_test_splitbyuser_41.txt'#'target_test_sample.txt'
         start_time = START_TIME_CCMR
         pred_time_train = 39
         pred_time_test = 40
