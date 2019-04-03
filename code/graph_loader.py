0cabb0a19a7a05d31c52a1355af8ee9a7acd04db
qinjr <qinjr@icloud.com>
2019-04-03 18:18:15 +0800

    minor

--- code/graph_loader.py
+++ code/graph_loader.py
@@ -318,7 +318,7 @@ class GraphLoader(object):
                     break
     
     def worker(self, params):
-        graph_handler = GraphHandler(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], params[8], params[9])
+        graph_handler = GraphHandler(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], params[8], params[9], params[10])
 
         while not (self.work.qsize() == 0 and self.producer_stop.value == 1):
             try:
