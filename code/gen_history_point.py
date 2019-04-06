d5d2cdc18da16de211235c39d1880c318aad507b
qinjr <qinjr@icloud.com>
2019-04-06 20:33:04 +0800

    minor

--- code/gen_history_point.py
+++ code/gen_history_point.py
@@ -11,6 +11,7 @@ DATA_DIR_CCMR = '../../score-data/CCMR/feateng/'
 START_TIME_CCMR = 1116432000
 START_TIME_IDX_CCMR = 30
 TIME_DELTA_CCMR = 90
+MAX_LEN_CCMR = 100
 
 def gen_user_item_hist_dict_ccmr(hist_file, user_hist_dict_file, item_hist_dict_file, pred_time=40):
     user_hist_dict = {}
@@ -58,7 +59,49 @@ def gen_user_item_hist_dict_ccmr(hist_file, user_hist_dict_file, item_hist_dict_
     with open(item_hist_dict_file, 'wb') as f:
         pkl.dump(item_hist_dict_sort, f)
 
+def gen_user_hist_seq_file(in_file, out_file, user_hist_dict_file):
+    with open(user_hist_dict_file, 'rb') as f:
+        user_hist_dict = pkl.load(f)
+    newlines = []
+    empty_user = []
+    with open(in_file, 'r') as f:
+        uid = line[:-1].split(',')[0]
+        if uid in user_hist_dict:
+            user_hist_list = user_hist_dict[uid]
+            if len(user_hist_list) > MAX_LEN_CCMR:
+                user_hist_list = user_hist_list[-MAX_LEN_CCMR:]
+        else:
+            user_hist_list = ['0']
+            empty_user.append(uid)
+        newlines.append(','.join(user_hist_list) + '\n')
+    with open(out_file, 'w') as f:
+        f.writelines(newlines)
+    print(len(empty_user))
+
+def gen_item_hist_seq_file(in_file, out_file, item_hist_dict_file):
+    with open(item_hist_dict_file, 'rb') as f:
+        item_hist_dict = pkl.load(f)
+    newlines = []
+    empty_item = []
+    with open(in_file, 'r') as f:
+        iid = line[:-1].split(',')[1]
+        if iid in item_hist_dict:
+            item_hist_list = item_hist_dict[iid]
+            if len(item_hist_list) > MAX_LEN_CCMR:
+                item_hist_list = item_hist_list[-MAX_LEN_CCMR:]
+        else:
+            item_hist_list = ['0']
+            empty_item.append(iid)
+        newlines.append(','.join(item_hist_list) + '\n')
+    with open(out_file, 'w') as f:
+        f.writelines(newlines)
+    print(len(empty_item))
 
 if __name__ == "__main__":
-    gen_user_item_hist_dict_ccmr(DATA_DIR_CCMR + 'rating_pos.csv', DATA_DIR_CCMR + 'user_hist_dict.pkl', DATA_DIR_CCMR + 'item_hist_dict.pkl')
+    # gen_user_item_hist_dict_ccmr(DATA_DIR_CCMR + 'rating_pos.csv', DATA_DIR_CCMR + 'user_hist_dict.pkl', DATA_DIR_CCMR + 'item_hist_dict.pkl')
+    gen_user_hist_seq_file(DATA_DIR_CCMR + 'target_train_splitbyuser_40.txt', DATA_DIR_CCMR + 'train_user_hist_seq.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl')
+    gen_user_hist_seq_file(DATA_DIR_CCMR + 'target_test_splitbyuser_40.txt', DATA_DIR_CCMR + 'test_user_hist_seq.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl')
+
+    gen_user_hist_seq_file(DATA_DIR_CCMR + 'target_train_splitbyuser_40.txt', DATA_DIR_CCMR + 'train_item_hist_seq.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl')
+    gen_user_hist_seq_file(DATA_DIR_CCMR + 'target_test_splitbyuser_40.txt', DATA_DIR_CCMR + 'test_item_hist_seq.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl')
 
