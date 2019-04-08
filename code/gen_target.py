9a90fd389007aeed742c4266dd35bc99d39919f5
qinjr <qinjr@icloud.com>
2019-04-08 17:14:38 +0800

    gen target4taobao

--- code/gen_target.py
+++ code/gen_target.py
@@ -6,40 +6,63 @@ import numpy as np
 import multiprocessing
 
 NEG_SAMPLE_NUM = 9
-WORKER_N = 5
-DATA_DIR_CCMR = '../../score-data/CCMR/feateng/'
 
 # CCMR dataset parameters
+DATA_DIR_CCMR = '../../score-data/CCMR/feateng/'
 TIME_SLICE_NUM_CCMR = 41
 OBJ_PER_TIME_SLICE_CCMR = 10
 USER_NUM_CCMR = 4920695
 ITEM_NUM_CCMR = 190129
-USER_PER_COLLECTION = 1000
-ITEM_PER_COLLECTION = 100
+USER_PER_COLLECTION_CCMR = 1000
+ITEM_PER_COLLECTION_CCMR = 100
 START_TIME_CCMR = 1116432000
 START_TIME_IDX_CCMR = 30
 TIME_DELTA_CCMR = 90
+
+# Taobao dataset parameters
+DATA_DIR_Taobao = '../../score-data/Taobao/feateng/'
+TIME_SLICE_NUM_Taobao = 9
+OBJ_PER_TIME_SLICE_Taobao = 10
+USER_NUM_Taobao = 984105
+ITEM_NUM_Taobao = 4067842
+USER_PER_COLLECTION_Taobao = 500
+ITEM_PER_COLLECTION_Taobao = 1000
+START_TIME_Taobao = int(time.mktime(datetime.datetime.strptime('2017-11-25', "%Y-%m-%d").timetuple()))
+START_TIME_IDX_Taobao = 0
+TIME_DELTA_Taobao = 1
+
 class TargetGen(object):
-    def __init__(self, user_neg_dict_file, db_name):
-        with open(user_neg_dict_file, 'rb') as f:
-            self.user_neg_dict = pkl.load(f)  
-        
+    def __init__(self, user_neg_dict_file, db_name, user_num, item_num, user_per_collection,
+                item_per_collection, start_time, start_time_idx, time_delta):
+        if user_neg_dict_file != None
+            with open(user_neg_dict_file, 'rb') as f:
+                self.user_neg_dict = pkl.load(f)  
+        else:
+            self.user_neg_dict = {}
+
         url = "mongodb://localhost:27017/"
         client = pymongo.MongoClient(url)
         db = client[db_name]
-        self.user_num = USER_NUM_CCMR
-        self.item_num = ITEM_NUM_CCMR
+        self.user_num = user_num
+        self.item_num = item_num
         
-        user_coll_num = self.user_num // USER_PER_COLLECTION
-        if self.user_num % USER_PER_COLLECTION != 0:
+        self.user_per_collection = user_per_collection
+        self.item_per_collection = item_per_collection
+
+        user_coll_num = self.user_num // self.user_per_collection
+        if self.user_num % self.user_per_collection != 0:
             user_coll_num += 1
-        item_coll_num = self.item_num // ITEM_PER_COLLECTION
-        if self.item_num % ITEM_PER_COLLECTION != 0:
+        item_coll_num = self.item_num // self.item_per_collection
+        if self.item_num % self.item_per_collection != 0:
             item_coll_num += 1
 
         self.user_colls = [db['user_%d'%(i)] for i in range(user_coll_num)]
         self.item_colls = [db['item_%d'%(i)] for i in range(item_coll_num)]
         
+        self.start_time = start_time
+        self.start_time_idx = start_time_idx
+        self.time_delta = time_delta
+
     def gen_user_neg_items(self, uid, neg_sample_num, iid_start, iid_end):
         if str(uid) in self.user_neg_dict:
             user_neg_list = self.user_neg_dict[str(uid)]
@@ -72,8 +95,8 @@ class TargetGen(object):
         with open(target_file, 'w') as f:
             f.writelines(target_lines)
         print('generate {} completed'.format(target_file))
-    
-    def gen_user_item_hist_dict_ccmr(self, hist_file, user_hist_dict_file, item_hist_dict_file, pred_time=40):
+
+    def gen_user_item_hist_dict(self, hist_file, user_hist_dict_file, item_hist_dict_file, pred_time):
         user_hist_dict = {}
         item_hist_dict = {}
 
@@ -81,11 +104,9 @@ class TargetGen(object):
         with open(hist_file, 'r') as f:
             for line in f:
                 uid, iid, _, time_str = line[:-1].split(',')
-                uid = str(int(uid) + 1)
-                iid = str(int(iid) + 1 + self.user_num)
-                time_int = int(time.mktime(datetime.datetime.strptime(time_str, "%Y-%m-%d").timetuple()))
-                time_idx = int((time_int - START_TIME_CCMR) / (SECONDS_PER_DAY * TIME_DELTA_CCMR))
-                if time_idx < START_TIME_IDX_CCMR:
+                time_int = int(time_str)
+                time_idx = int((time_int - self.start_time) / (SECONDS_PER_DAY * self.time_delta))
+                if time_idx < self.start_time_idx:
                     continue
                 if time_idx >= pred_time:
                     continue
@@ -140,6 +161,22 @@ class TargetGen(object):
         print('filter target file completed')   
 
 if __name__ == '__main__':
-    tg = TargetGen(DATA_DIR_CCMR + 'user_neg_dict.pkl', 'ccmr_1hop')
-    tg.filter_target_file(DATA_DIR_CCMR + 'target_40.txt', DATA_DIR_CCMR + 'target_40_hot.txt', DATA_DIR_CCMR + 'target_40_cold.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl')
+    # # CCMR
+    # tg = TargetGen(DATA_DIR_CCMR + 'user_neg_dict.pkl', 'ccmr_1hop', user_num = USER_NUM_CCMR,
+    #             item_num = ITEM_NUM_CCMR, user_per_collection = USER_PER_COLLECTION_CCMR,
+    #             item_per_collection = ITEM_PER_COLLECTION_CCMR, start_time = START_TIME_CCMR, 
+    #             start_time_idx = START_TIME_IDX_CCMR, time_delta = TIME_DELTA_CCMR)
+    # tg.gen_target_file(NEG_SAMPLE_NUM, DATA_DIR_CCMR + 'target_40.txt', 40)
+    # tf.gen_user_item_hist_dict(DATA_DIR_CCMR + 'remap_rating_pos_idx.csv', DATA_DIR_CCMR + 'user_hist_dict.pkl', DATA_DIR_CCMR + 'item_hist_dict.pkl', 40)
+    # tg.filter_target_file(DATA_DIR_CCMR + 'target_40.txt', DATA_DIR_CCMR + 'target_40_hot.txt', DATA_DIR_CCMR + 'target_40_cold.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl')
+    
+    # Taobao
+    tg = TargetGen(None, 'taobao_1hop', user_num = USER_NUM_Taobao,
+                item_num = ITEM_NUM_Taobao, user_per_collection = USER_PER_COLLECTION_Taobao,
+                item_per_collection = ITEM_PER_COLLECTION_Taobao, start_time = START_TIME_Taobao, 
+                start_time_idx = START_TIME_IDX_Taobao, time_delta = TIME_DELTA_Taobao)
+    tg.gen_target_file(NEG_SAMPLE_NUM, DATA_DIR_Taobao + 'target_8.txt', 8)
+    tf.gen_user_item_hist_dict(DATA_DIR_Taobao + 'remaped_user_behavior.txt', DATA_DIR_Taobao + 'user_hist_dict.pkl', DATA_DIR_Taobao + 'item_hist_dict.pkl', 8)
+    tg.filter_target_file(DATA_DIR_Taobao + 'target_8.txt', DATA_DIR_Taobao + 'target_8_hot.txt', DATA_DIR_Taobao + 'target_8_cold.txt', DATA_DIR_Taobao + 'user_hist_dict.pkl')
+
     
