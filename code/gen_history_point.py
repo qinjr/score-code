0b526aca7bb767c6c791c570a6db18b447553b3d
qinjr <qinjr@icloud.com>
2019-04-08 17:19:25 +0800

    gen history4taobao

--- code/gen_history_point.py
+++ code/gen_history_point.py
@@ -4,13 +4,15 @@ import time
 import numpy as np
 import datetime
 
-SECONDS_PER_DAY = 24 * 3600
-
 # CCMR dataset parameters
 DATA_DIR_CCMR = '../../score-data/CCMR/feateng/'
 MAX_LEN_CCMR = 100
 
-def gen_user_hist_seq_file(in_file, out_file, user_hist_dict_file):
+# CCMR dataset parameters
+DATA_DIR_CCMR = '../../score-data/Taobao/feateng/'
+MAX_LEN_Taobao = 100
+
+def gen_user_hist_seq_file(in_file, out_file, user_hist_dict_file, max_len):
     with open(user_hist_dict_file, 'rb') as f:
         user_hist_dict = pkl.load(f)
     newlines = []
@@ -19,15 +21,15 @@ def gen_user_hist_seq_file(in_file, out_file, user_hist_dict_file):
             uid = line[:-1].split(',')[0]
             if uid in user_hist_dict:
                 user_hist_list = user_hist_dict[uid]
-                if len(user_hist_list) > MAX_LEN_CCMR:
-                    user_hist_list = user_hist_list[-MAX_LEN_CCMR:]
+                if len(user_hist_list) > max_len:
+                    user_hist_list = user_hist_list[-max_len:]
             else:
                 user_hist_list = ['0']
             newlines.append(','.join(user_hist_list) + '\n')
     with open(out_file, 'w') as f:
         f.writelines(newlines)
 
-def gen_item_hist_seq_file(in_file, out_file, item_hist_dict_file):
+def gen_item_hist_seq_file(in_file, out_file, item_hist_dict_file, max_len):
     with open(item_hist_dict_file, 'rb') as f:
         item_hist_dict = pkl.load(f)
     newlines = []
@@ -37,8 +39,8 @@ def gen_item_hist_seq_file(in_file, out_file, item_hist_dict_file):
             for iid in iids:
                 if iid in item_hist_dict:
                     item_hist_list = item_hist_dict[iid]
-                    if len(item_hist_list) > MAX_LEN_CCMR:
-                        item_hist_list = item_hist_list[-MAX_LEN_CCMR:]
+                    if len(item_hist_list) > max_len:
+                        item_hist_list = item_hist_list[-max_len:]
                 else:
                     item_hist_list = ['0']
                 newlines.append(','.join(item_hist_list) + '\n')
@@ -47,9 +49,19 @@ def gen_item_hist_seq_file(in_file, out_file, item_hist_dict_file):
 
 
 if __name__ == "__main__":
-    gen_user_hist_seq_file(DATA_DIR_CCMR + 'target_40_hot_train.txt', DATA_DIR_CCMR + 'train_user_hist_seq.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl')
-    gen_user_hist_seq_file(DATA_DIR_CCMR + 'target_40_hot_test.txt', DATA_DIR_CCMR + 'test_user_hist_seq.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl')
+    # # CCMR
+    # gen_user_hist_seq_file(DATA_DIR_CCMR + 'target_40_hot_train.txt', DATA_DIR_CCMR + 'train_user_hist_seq.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl', MAX_LEN_CCMR)
+    # gen_user_hist_seq_file(DATA_DIR_CCMR + 'target_40_hot_test.txt', DATA_DIR_CCMR + 'test_user_hist_seq.txt', DATA_DIR_CCMR + 'user_hist_dict.pkl', MAX_LEN_CCMR)
 
-    gen_item_hist_seq_file(DATA_DIR_CCMR + 'target_40_hot_train.txt', DATA_DIR_CCMR + 'train_item_hist_seq.txt', DATA_DIR_CCMR + 'item_hist_dict.pkl')
-    gen_item_hist_seq_file(DATA_DIR_CCMR + 'target_40_hot_test.txt', DATA_DIR_CCMR + 'test_item_hist_seq.txt', DATA_DIR_CCMR + 'item_hist_dict.pkl')
+    # gen_item_hist_seq_file(DATA_DIR_CCMR + 'target_40_hot_train.txt', DATA_DIR_CCMR + 'train_item_hist_seq.txt', DATA_DIR_CCMR + 'item_hist_dict.pkl', MAX_LEN_CCMR)
+    # gen_item_hist_seq_file(DATA_DIR_CCMR + 'target_40_hot_test.txt', DATA_DIR_CCMR + 'test_item_hist_seq.txt', DATA_DIR_CCMR + 'item_hist_dict.pkl', MAX_LEN_CCMR)
+
+
+    # Taobao
+    gen_user_hist_seq_file(DATA_DIR_Taobao + 'target_8_hot_train.txt', DATA_DIR_Taobao + 'train_user_hist_seq.txt', DATA_DIR_Taobao + 'user_hist_dict.pkl', MAX_LEN_Taobao)
+    gen_user_hist_seq_file(DATA_DIR_Taobao + 'target_8_hot_test.txt', DATA_DIR_Taobao + 'test_user_hist_seq.txt', DATA_DIR_Taobao + 'user_hist_dict.pkl', MAX_LEN_Taobao)
+
+    gen_item_hist_seq_file(DATA_DIR_Taobao + 'target_8_hot_train.txt', DATA_DIR_Taobao + 'train_item_hist_seq.txt', DATA_DIR_Taobao + 'item_hist_dict.pkl', MAX_LEN_Taobao)
+    gen_item_hist_seq_file(DATA_DIR_Taobao + 'target_8_hot_test.txt', DATA_DIR_Taobao + 'test_item_hist_seq.txt', DATA_DIR_Taobao + 'item_hist_dict.pkl', MAX_LEN_Taobao)
+
 
