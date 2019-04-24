b62a52bdef9c9474c6118e8cdaa693bb059db429
qinjr <qinjr@icloud.com>
2019-04-24 16:43:45 +0800

    resplit train/test

---
+++ code/sampling.py
@@ -0,0 +1,44 @@
+import random
+import sys
+
+# CCMR dataset parameters
+DATA_DIR_CCMR = '../../score-data/CCMR/feateng/'
+
+# Taobao dataset parameters
+DATA_DIR_Taobao = '../../score-data/Taobao/feateng/'
+
+# Tmall dataset parameters
+DATA_DIR_Tmall = '../../score-data/Tmall/feateng/'
+
+def sample_files(target_file, user_seq_file, sample_target_file, sample_user_seq_file,
+                sample_factor):
+    target_lines = open(target_file).readlines()
+    user_seq_lines = open(user_seq_file).readlines()
+
+    sample_target_lines = []
+    sample_user_seq_lines = []
+    
+    length = len(target_lines)
+    for i in range(length):
+        rand_int = random.randint(1, sample_factor)
+        if rand_int == 1:
+            sample_target_lines.append(target_lines[i])
+            sample_user_seq_lines.append(user_seq_lines[i])
+    with open(sample_target_file, 'w') as f:
+        f.writelines(sample_target_lines)
+    with open(sample_user_seq_file, 'w') as f:
+        f.writelines(sample_user_seq_lines)
+
+
+if __name__ == 'main':
+    if len(sys.argv) < 2:
+        print("PLEASE INPUT [DATASET]")
+        sys.exit(0)
+    dataset = sys.argv[1]
+    if dataset == 'CCMR':
+        # CCMR
+        sample_files(DATA_DIR_CCMR + 'target_39_hot.txt', DATA_DIR_CCMR + 'train_user_hist_seq_39.txt', DATA_DIR_CCMR + 'target_39_hot_sample.txt', DATA_DIR_CCMR + 'train_user_hist_seq_39_sample.txt', 2)
+        sample_files(DATA_DIR_CCMR + 'target_40_hot.txt', DATA_DIR_CCMR + 'test_user_hist_seq_40.txt', DATA_DIR_CCMR + 'target_40_hot_sample.txt', DATA_DIR_CCMR + 'test_user_hist_seq_40_sample.txt', 3)
+    else:
+        print('WRONG DATASET: {}'.format(dataset))
+
