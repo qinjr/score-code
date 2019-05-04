a1f616cf4d56a82c7368445577fb04d59e56a32d
qinjr <qinjr@icloud.com>
2019-05-04 21:24:10 +0800

    SCORE_GAT_ATT

--- code/score/train_score.py
+++ code/score/train_score.py
@@ -76,6 +76,8 @@ def restore(data_set, target_file_test, graph_handler_params, start_time,
         model = SCORE_NEW_BASE(feature_size, eb_dim, hidden_size, max_time_len, obj_per_time_slice)
     elif model_type == 'SCORE_GAT':
         model = SCORE_GAT(feature_size, eb_dim, hidden_size, max_time_len, obj_per_time_slice)
+    elif model_type == 'SCORE_GAT_ATT':
+        model = SCORE_GAT_ATT(feature_size, eb_dim, hidden_size, max_time_len, obj_per_time_slice)
     else:
         print('WRONG MODEL TYPE')
         exit(1)
@@ -191,6 +193,8 @@ def train(data_set, target_file_train, target_file_test, graph_handler_params, s
         model = SCORE_NEW_BASE(feature_size, eb_dim, hidden_size, max_time_len, obj_per_time_slice)
     elif model_type == 'SCORE_GAT':
         model = SCORE_GAT(feature_size, eb_dim, hidden_size, max_time_len, obj_per_time_slice)
+    elif model_type == 'SCORE_GAT_ATT':
+        model = SCORE_GAT_ATT(feature_size, eb_dim, hidden_size, max_time_len, obj_per_time_slice)
     else:
         print('WRONG MODEL TYPE')
         exit(1)
