cb199d562f702352b5804a634dacdef48527e459
qinjr <qinjr@icloud.com>
2019-05-04 18:12:06 +0800

    SCORE_GAT

--- code/score/score.py
+++ code/score/score.py
@@ -583,3 +583,57 @@ class SCORE_ATT_GAT(SCOREBASE):
         seq2_result = tf.reduce_sum(seq2 * seq2_weights, axis=2)
 
         return seq1_result, seq2_result, seq1_weights, seq2_weights, affinity
+
+class SCORE_GAT(SCOREBASE):
+    def __init__(self, feature_size, eb_dim, hidden_size, max_time_len, 
+                obj_per_time_slice):
+        super(SCORE_GAT, self).__init__(feature_size, eb_dim, hidden_size, max_time_len, obj_per_time_slice)
+        # HOP:0
+        self.target_user_t = tf.tile(tf.expand_dims(self.target_user, axis=1), [1, max_time_len, 1])
+        self.target_item_t = tf.tile(tf.expand_dims(self.target_item, axis=1), [1, max_time_len, 1])
+        user_side = self.target_user_t
+        item_side = self.target_item_t
+
+        # HOP:1
+        user_1hop_gat = self.gat(self.user_1hop, self.user_1hop, item_side)
+        item_1hop_gat = self.gat(self.item_1hop, self.item_1hop, user_side)
+        user_side += user_1hop_gat
+        item_side += item_1hop_gat
+        
+        # HOP:2
+        user_2hop_gat = self.gat(self.user_2hop, self.user_2hop, item_side)
+        item_2hop_gat = self.gat(self.item_2hop, self.item_2hop, user_side)
+        user_side += user_2hop_gat
+        item_side += item_2hop_gat
+        
+        # RNN
+        with tf.name_scope('rnn'):
+            user_side_rep_t, user_side_final_state = tf.nn.dynamic_rnn(GRUCell(hidden_size), inputs=user_side, 
+                                                        sequence_length=self.length_ph, dtype=tf.float32, scope='gru_user_side')
+            item_side_rep_t, item_side_final_state = tf.nn.dynamic_rnn(GRUCell(hidden_size), inputs=item_side, 
+                                                        sequence_length=self.length_ph, dtype=tf.float32, scope='gru_item_side')
+
+        inp = tf.concat([user_side_final_state, item_side_final_state, self.target_user, self.target_item], axis=1)
+
+        # fc layer
+        self.build_fc_net(inp)
+        # build loss
+        self.build_logloss()
+        self.build_l2norm()
+        self.auxloss = self.loss
+        self.build_train_step()
+
+    def lrelu(self, x, alpha=0.2):
+        return tf.nn.relu(x) - alpha * tf.nn.relu(-x)
+        
+    def gat(self, key, value, query):
+        # key/value: [B, T, K, D], query: [B, T, D]
+        key_shape = key.get_shape().as_list()
+        query = tf.expand_dims(query, 2) # [B, T, 1, D]
+        query = tf.tile(query, [1, 1, key_shape[2], 1]) #[B, T, K, D]
+        query_key_concat = tf.concat([query, key], axis = 3)
+        atten = tf.layers.dense(query_key_concat, 1, activation=None, use_bias=False) #[B, T, K, 1]
+        atten = self.lrelu(atten)
+        atten = tf.nn.softmax(atten, dim=2) #[B, T, K, 1]
+        res = tf.reduce_sum(atten * value, axis=2)
+        return res
