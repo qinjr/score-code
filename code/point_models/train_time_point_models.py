e60849d0986ddf927f8209ae4336cfd4c64ac544
qinjr <qinjr@icloud.com>
2019-05-05 15:30:20 +0800

    minor

--- code/point_models/train_time_point_models.py
+++ code/point_models/train_time_point_models.py
@@ -295,7 +295,7 @@ if __name__ == '__main__':
 
     ################################## training hyper params ##################################
     reg_lambda = 1e-3
-    hyper_paras = [(100, 5e-4), (200, 1e-3)]
+    hyper_paras = [(100, 5e-5), (200, 1e-4)]#[(100, 5e-4), (200, 1e-3)]
 
     for hyper in hyper_paras:
         train_batch_size, lr = hyper
